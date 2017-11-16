47c804286ec144a9daab805ce5ac6ff941dbc861
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-17 01:05:20 +0200

    Wrote index route and controller for testing.

--- src/Kudze/Nfq/WeatherBundle/Controller/DefaultController.php
+++ src/Kudze/Nfq/WeatherBundle/Controller/DefaultController.php
@@ -2,12 +2,21 @@
 
 namespace Kudze\Nfq\WeatherBundle\Controller;
 
+use Kudze\Nfq\WeatherBundle\Core\Location;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('NfqWeatherBundle:Default:index.html.twig');
+
+        $location = new Location(54.68, 25.27);
+        $provider = $this->container->get('nfq_weather.provider');
+        $weather = $provider->fetch($location);
+
+        return $this->render('NfqWeatherBundle:Default:index.html.twig', [
+            'location' => $location,
+            'weather' => $weather
+        ]);
     }
 }
