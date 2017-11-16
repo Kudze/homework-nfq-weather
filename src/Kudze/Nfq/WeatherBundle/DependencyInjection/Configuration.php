ecc7268cce5c1885af031bb6df06168696050d9f
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-16 23:29:52 +0200

    Some optimatization

--- src/Kudze/Nfq/WeatherBundle/DependencyInjection/Configuration.php
+++ src/Kudze/Nfq/WeatherBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('nfq_weather')
+        $treeBuilder->root('nfq_weather')
             ->children()
 
                 ->enumNode('provider')
